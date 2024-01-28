2289c32f45eaabf8a574b618b1e916b89ce55530
weward <dev.weward@gmail.com>
2024-01-28 19:05:24 +0800

    Added DB Seeder for Task

---
+++ api/database/seeders/TaskSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Task;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TaskSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Create a task that has been done
+        Task::factory()->done()->count(2)->create();
+
+        // Create a task that is still active
+        Task::factory()->active()->count(2)->create();
+    }
+}
